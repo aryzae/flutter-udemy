7ff404055a0a81a5549f8980bdf39edcb633d1de
shoito <a.aryzae@gmail.com>
2020-05-24 16:50:54 +0900

    Change my card

--- mi_card_flutter/lib/main.dart
+++ mi_card_flutter/lib/main.dart
@@ -9,17 +9,17 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.teal,
+        backgroundColor: Colors.blueAccent,
         body: SafeArea(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
               CircleAvatar(
                 radius: 50.0,
-                backgroundImage: AssetImage('images/angela.jpg'),
+                backgroundImage: AssetImage('images/aryzae.jpg'),
               ),
               Text(
-                'Angela Yu',
+                'Aryzae',
                 style: TextStyle(
                   fontFamily: 'Pacifico',
                   fontSize: 40.0,
@@ -28,53 +28,53 @@ class MyApp extends StatelessWidget {
                 ),
               ),
               Text(
-                'FLUTTER DEVELOPER',
+                'iOS & Flutter Developer',
                 style: TextStyle(
                   fontFamily: 'Source Sans Pro',
                   fontSize: 20.0,
-                  color: Colors.teal.shade100,
+                  color: Colors.blue.shade100,
                   letterSpacing: 2.5,
                   fontWeight: FontWeight.bold,
                 ),
               ),
               SizedBox(
-                width: 150.0,
-                height: 20.0,
+                width: 200.0,
+                height: 16.0,
                 child: Divider(
-                  color: Colors.teal.shade100,
+                  color: Colors.blue.shade100,
                 ),
               ),
               Card(
                 color: Colors.white,
-                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                margin: EdgeInsets.symmetric(vertical: 8.0, horizontal: 24.0),
                 child: ListTile(
                     leading: Icon(
                       Icons.phone,
-                      color: Colors.teal,
+                      color: Colors.blueAccent,
                     ),
                     title: Text(
-                      '+44 123 456 789',
+                      '+81 000 1234 5678',
                       style: TextStyle(
                         fontFamily: 'Source Sans Pro',
                         fontSize: 20.0,
-                        color: Colors.teal.shade900,
+                        color: Colors.blue.shade500,
                       ),
                     )),
               ),
               Card(
                 color: Colors.white,
-                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                margin: EdgeInsets.symmetric(vertical: 8.0, horizontal: 24.0),
                 child: ListTile(
                   leading: Icon(
                     Icons.email,
-                    color: Colors.teal,
+                    color: Colors.blueAccent,
                   ),
                   title: Text(
-                    'angela@email.com',
+                    'example@email.com',
                     style: TextStyle(
                       fontFamily: 'Source Sans Pro',
                       fontSize: 20.0,
-                      color: Colors.teal.shade900,
+                      color: Colors.blue.shade500,
                     ),
                   ),
                 ),
