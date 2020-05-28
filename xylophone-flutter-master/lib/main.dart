b0ccbb5696989ffaaf29f6bfa968c9a2c0c59699
shoito <a.aryzae@gmail.com>
2020-05-29 02:20:35 +0900

    Impl Xylophone project

--- xylophone-flutter-master/lib/main.dart
+++ xylophone-flutter-master/lib/main.dart
@@ -1,15 +1,54 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audio_cache.dart';
 
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
+  final color = [
+    Colors.red,
+    Colors.orange,
+    Colors.yellow,
+    Colors.green,
+    Colors.teal,
+    Colors.blue,
+    Colors.purple,
+  ];
+
+  void playSound(int soundNumber) {
+    final player = AudioCache();
+    player.play('note$soundNumber.wav');
+  }
+
+  Expanded buildKey(int index) {
+    return Expanded(
+      child: FlatButton(
+        color: color[index],
+        onPressed: () {
+          playSound(index + 1);
+        },
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.black,
         body: SafeArea(
-          child: Container(),
-        ),
+            child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            buildKey(0),
+            buildKey(1),
+            buildKey(2),
+            buildKey(3),
+            buildKey(4),
+            buildKey(5),
+            buildKey(6),
+          ],
+        )),
       ),
     );
   }
